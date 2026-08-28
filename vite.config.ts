34cee7e9c1e7a77d1db2eba2ac57543caaf920d5
Zhou Jianyong <zhoujianyong@users.noreply.github.com>
2026-08-28 21:51:43 +0800

    build: 配置 GitHub Pages base 路径

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/teemaker-3d-teaching/',
   plugins: [react()],
   server: {
     host: true,
